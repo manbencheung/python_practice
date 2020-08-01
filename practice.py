3f243ae8fa6b71d5e2bb03983a7db6eacb8f89c4
Ben <cheungmanben@gmail.com>
2020-08-01 19:05:29 +1000

    practice

---
+++ practice.py
@@ -0,0 +1,60 @@
+#################  question 1 ####################
+# for a in range(2000,2100):
+#     if (a%7==0) and (a%5!=0):
+#         print(a,end=" ")
+#
+# print("")
+
+
+################  question2  #########################
+# while True:
+#     x,y,z = map(int,input("hour,min,sec:\n").split())
+#     a = x*3600
+#     b = y*60
+#
+#     if x is y is z == 0:
+#                 break
+#
+#     print(a + b + z)
+
+
+###################  question 3 ####################
+
+# a = int(input("num:"))
+# if a >0 :
+#     print("True")
+# else:print("False")
+
+
+###################  problem 4  ########################
+# num = input("input: ").split();
+# print('output: ', (((int((num[0][0])) - int((num[1][0])) < 0) or ((int((num[1])) / int((num[0])) >= 2))) and
+#                    ((int(num[1])) not in range(-8, 16))) or (int(num[1]) == 0))
+
+###############  prob 6#############
+
+# while True:
+#     a  = int(input("enter a number(n):"))
+#     num = 0
+#     if a !=0:
+#             for  i  in range(1, a+1):
+#                 num += i
+#             print(num)
+#
+#     if a == 0:
+#         break
+#########################  problem 5  ####################
+
+# num = input("input:").split()
+# num.sort()
+# print("output:",((int(num[1])-int(num[0]))==(int(num[2])-int(num[1]))))
+
+
+############################# problem 8  ##########################
+# a,b =eval(input("input"))
+# x =a[-1:]
+# y =b[0:1]
+# if x==y :
+#     print("output:","'" + a.strip(y) + b + "'")
+# else:
+#     print("'" + a + b + "'")
